69c6aa555ecfc54bf82be2beb7693778b4d98845
Aby Bandoh <aby.bandoh@gmail.com>
2013-10-24 15:49:43 -0400

    another db migration change

--- db/migrate/20131022225329_create_friendships.rb
+++ db/migrate/20131022225329_create_friendships.rb
@@ -7,16 +7,5 @@ class CreateFriendships < ActiveRecord::Migration
       t.timestamps
     end
 
-    reversible do |dir|
-      dir.up do
-        execute <<-SQL  
-          ALTER TABLE posts
-          ADD CONSTRAINT fk_friendship_constraint
-          FOREIGN KEY (friendship_id)
-          REFERENCES friendships(id)
-        SQL
-      end
-    end
-
   end
 end
